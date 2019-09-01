327152555ce11830f7cbc4ad757a6e3d0f9c0d66
podyankee <podyankee@gmail.com>
2019-09-01 06:19:56 +0300

    js promises

--- src/js/common.js
+++ src/js/common.js
@@ -5,34 +5,69 @@ function apiSearch (e) {
   e.preventDefault()
   const searchText = document.querySelector('.form-control').value
   const server = 'https://api.themoviedb.org/3/search/multi?api_key=1352084e037071184a371f99fee87cd8&language=ru&query=' + searchText
+  movie.innerHTML = 'Загрузка'
   requestApi(server)
+    .then((result) => {
+      const output = JSON.parse(result)
+      let inner = ''
+
+      output.results.forEach(function (item) {
+        let nameItem = item.name || item.title
+        console.log(nameItem)
+        inner += '<div class="col-3">' + nameItem + '</div>'
+      })
+
+      movie.innerHTML = inner
+    })
+    .catch((reason) => {
+      movie.innerHTML = 'Упс, что-то пошло не так !'
+      console.log('error' + reason.status)
+    })
 }
 
 searchForm.addEventListener('submit', apiSearch)
 
 function requestApi (url) {
-  const request = new XMLHttpRequest()
-  request.open('GET', url)
-  request.send()
-
-  request.addEventListener('readystatechange', () => {
-    if (request.readyState !== 4) return
-
-    if (request.status !== 200) {
-      console.log('error' + request.status)
-      return
-    }
-
-    const output = JSON.parse(request.responseText)
-
-    let inner = ''
-
-    output.results.forEach(function (item) {
-      let nameItem = item.name || item.title
-      console.log(nameItem)
-      inner += '<div class="col-3">' + nameItem + '</div>'
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest()
+    request.open('GET', url)
+    request.addEventListener('load', () => {
+      if (request.status !== 200) {
+        reject({
+          status: request.status
+        })
+        return
+      }
+      resolve(request.response)
     })
-
-    movie.innerHTML = inner
+    request.addEventListener('error', () => {
+      reject({
+        status: request.status
+      })
+    })
+    request.send()
   })
 }
+
+//   request.addEventListener('readystatechange', () => {
+//     if (request.readyState !== 4) {
+//       movie.innerHTML = 'Загрузка'
+//       return
+//     }
+
+//     if (request.status !== 200) {
+//       movie.innerHTML = 'Упс, что-то пошло не так !'
+//       console.log('error' + request.status)
+//       return
+//     }
+
+//     let inner = ''
+
+//     output.results.forEach(function (item) {
+//       let nameItem = item.name || item.title
+//       console.log(nameItem)
+//       inner += '<div class="col-3">' + nameItem + '</div>'
+//     })
+
+//     movie.innerHTML = inner
+//   })
